db9b4a551449ed06c46ea98eec4104149d23ed46
Kyle Naumann <67936151+kyle-naumann@users.noreply.github.com>
2021-11-16 12:23:14 -0800

    Built Register, Login, Nav

--- codrills/src/App.js
+++ codrills/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Register from './Components/Register';
+import Login from './Components/Login'
+import Navigation from './Components/Navigation'
+import Home from './Components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<Navigation/>
+    <Switch>
+      <Route path="/" render={() => <Home/>}/>
+      <Route exact path="/" render={() => <Login/>}/>
+      <Route path="/register" render={() => <Register/>}/>
+    </Switch>
+
     </div>
+
   );
 }
 
