081bc87aae4690f7ee1341ef45c607aa5997cce2
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-11-18 21:00:03 -0700

    wip

--- codrills/src/App.js
+++ codrills/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Register from './Components/Register';
 import Login from './Components/Login'
 import Navigation from './Components/Navigation'
@@ -9,11 +9,11 @@ function App() {
   return (
     <div>
 <Navigation/>
-    <Switch>
+    <Routes>
       <Route path="/" render={() => <Home/>}/>
       <Route exact path="/" render={() => <Login/>}/>
       <Route path="/register" render={() => <Register/>}/>
-    </Switch>
+    </Routes>
 
     </div>
 
